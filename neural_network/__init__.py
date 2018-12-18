4828e469e744cbdbcc93457081b2597f207343a3
kalinkinisaac <kalinkin.isaac@icloud.com>
2018-12-18 14:41:26 +0300

    hueta

--- neural_network/__init__.py
+++ neural_network/__init__.py
@@ -18,7 +18,5 @@ fields = dict({
 })
 
 # This code will create database file in root directory of the project
-db.init(fields=fields)
-db.load_data("data.txt")
 
 
--- neural_network/training_data.py
+++ neural_network/training_data.py
@@ -3,5 +3,5 @@ from database import db
 class Tutorials:
     @staticmethod
     def get_sample(sample_id):
-        query = db.get_row(sample_id)[0][2::]
-        return {"inputs" : query[::-1], "answer" : [query[-1]]}
+        query = db.get_row(sample_id)[0][2:]
+        return {"inputs" : query[:-1], "answer" : [query[-1]]}
