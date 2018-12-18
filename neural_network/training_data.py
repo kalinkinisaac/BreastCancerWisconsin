201c25e11af69c5cd66ef7187d9ce44520bd006e
kalinkinisaac <kalinkin.isaac@icloud.com>
2018-12-18 15:21:50 +0300

    blat

--- neural_network/training_data.py
+++ neural_network/training_data.py
@@ -1,5 +1,7 @@
 from database import db
 from . import sigmoid
+
+
 class Tutorials:
     @staticmethod
     def get_sample(sample_id):
@@ -7,4 +9,13 @@ class Tutorials:
         if '?' in query:
             raise ValueError('idi nahuy')
 
-        return {"inputs" : list(map(lambda x: x/10, query[:-1])), "answer" : [query[-1]/4]}
+        return {"inputs": list(map(lambda x: x / 10, query[:-1])), "answer": [query[-1] / 4]}
+
+    @staticmethod
+    def get_samples(id_min=1, id_max=500):
+        query = list(filter(lambda r: '?' not in r, db.get_rows(id_min, id_max)))
+        inputs, answers = [], []
+        for row in query:
+            inputs += [list(map(lambda x: x / 10, row[:-1]))]
+            answers += [row[-1] / 4]
+        return inputs, answers
